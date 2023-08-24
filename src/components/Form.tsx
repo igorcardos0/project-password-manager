3a7e13521d77382b1efcaa54e0fff3332c064991
Igor Cardoso <dev.igordesouzacardoso@gmail.com>
2023-08-24 18:38:03 -0300

    RESOLVE R5

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,64 +1,103 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
-interface FormProps {
+type FormProps = {
   onCancel: () => void;
-}
+};
 
 function Form({ onCancel }: FormProps) {
-  const [serviceName, setServiceName] = useState('');
-  const [login, setLogin] = useState('');
-  const [password, setPassword] = useState('');
-  const [url, setUrl] = useState('');
+  const [isButtonEnabled, setIsButtonEnabled] = useState<boolean | '' >(false);
+  const [passwordValidation, setPasswordValidation] = useState({
+    minLength: false,
+    maxLength: false,
+    hasLettersAndNumbers: false,
+    hasSpecialCharacter: false,
+  });
 
-  const isFormValid = serviceName !== ''
-    && login !== ''
-    && password.length >= 8
-    && password.length <= 16
-    && /\d/.test(password)
-    && /[a-zA-Z]/.test(password)
-    && /[^a-zA-Z0-9]/.test(password);
+  const validatePassword = (password: string) => {
+    const minLength = password.length >= 8;
+    const maxLength = password.length <= 16;
+    const hasLettersAndNumbers = /\d/.test(password) && /[a-zA-Z]/.test(password);
+    const hasSpecialCharacter = /[!@#$%^&*]/.test(password);
+
+    return {
+      minLength,
+      maxLength,
+      hasLettersAndNumbers,
+      hasSpecialCharacter,
+    };
+  };
+
+  const handleInputChange = () => {
+    const serviceName = document.getElementById('serviceName') as HTMLInputElement;
+    const login = document.getElementById('login') as HTMLInputElement;
+    const password = document.getElementById('password') as HTMLInputElement;
+
+    const newPasswordValidation = validatePassword(password.value);
+    setPasswordValidation(newPasswordValidation);
+
+    const isFormValid = serviceName.value
+      && login.value
+      && newPasswordValidation.minLength
+      && newPasswordValidation.maxLength
+      && newPasswordValidation.hasLettersAndNumbers
+      && newPasswordValidation.hasSpecialCharacter;
+
+    setIsButtonEnabled(isFormValid);
+  };
+
+  const renderPasswordMessage = (isValid: boolean, message: string) => {
+    return isValid ? (
+      <p className="valid-password-check">{message}</p>
+    ) : (
+      <p className="invalid-password-check">{message}</p>
+    );
+  };
 
   return (
-    <div className="form-container">
-      <div className="form-group">
-        <label htmlFor="service-name">Nome do serviço</label>
-        <input
-          type="text"
-          id="service-name"
-          value={ serviceName }
-          onChange={ (e) => setServiceName(e.target.value) }
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="login">Login</label>
-        <input
-          type="text"
-          id="login"
-          value={ login }
-          onChange={ (e) => setLogin(e.target.value) }
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password">Senha</label>
-        <input
-          type="password"
-          id="password"
-          value={ password }
-          onChange={ (e) => setPassword(e.target.value) }
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="url">URL</label>
-        <input
-          type="text"
-          id="url"
-          value={ url }
-          onChange={ (e) => setUrl(e.target.value) }
-        />
-      </div>
-      <button disabled={ !isFormValid }>Cadastrar</button>
+    <>
+      <label htmlFor="serviceName">Nome do serviço:</label>
+      <input
+        type="text"
+        id="serviceName"
+        name="serviceName"
+        onChange={ handleInputChange }
+      />
+      <label htmlFor="login">Login:</label>
+      <input type="text" id="login" name="login" onChange={ handleInputChange } />
+      <label htmlFor="password">Senha:</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        onChange={ handleInputChange }
+      />
+      <label htmlFor="url">URL:</label>
+      <input type="text" id="url" name="url" />
+
+      {renderPasswordMessage(
+        passwordValidation.minLength,
+        'Possuir 8 ou mais caracteres',
+      )}
+
+      {renderPasswordMessage(
+        passwordValidation.maxLength,
+        'Possuir até 16 caracteres',
+      )}
+
+      {renderPasswordMessage(
+        passwordValidation.hasLettersAndNumbers,
+        'Possuir letras e números',
+      )}
+
+      {renderPasswordMessage(
+        passwordValidation.hasSpecialCharacter,
+        'Possuir algum caractere especial',
+      )}
+
+      <button disabled={ isButtonEnabled === '' || !isButtonEnabled }>Cadastrar</button>
       <button onClick={ onCancel }>Cancelar</button>
-    </div>
+    </>
   );
 }
+
 export default Form;
